39631388b9cda412e0091b75ca23e82b2229019b
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-10 22:48:22 +0530

    (feat): stake tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -12,9 +12,38 @@ contract TokenFarm {
     MastiToken public mastiToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     // get masti and dai token
     constructor(MastiToken _mastiToken, DaiToken _daiToken) public {
         mastiToken = _mastiToken;
         daiToken = _daiToken;
     }
+
+    // Stake Tokens (Deposit)
+    function stakeTokens(uint256 _amount) public {
+        // Transfer mock dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add user to stakers array *only if haven't staked yet
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // Unstake Tokens (Withdraw)
+    // function unStakeTokens(uint256 _amount) public {}
+
+    // Issuing Tokens (Earn Intrest)
+    // function issueTokens(uint256 _amount) public {}
 }
